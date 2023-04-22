a7fa44603d2541dbc67019b7f6dd346b9bb39c27
Stefano Semeria <stefano.semeria@gmail.com>
2023-04-22 19:51:20 +0200

    prima versione con generazione ore

--- clock/clock.ino
+++ clock/clock.ino
@@ -1,4 +1,17 @@
 #include <Arduino.h>
+ #include <SPI.h>
+//For RTC support
+#include "RTClib.h"
+// Generic I2C support
+#include <Wire.h>
+
+#define UPDATE_PERIOD_MINUTES 2
+
+RTC_DS1307 rtc;
+
+//Global array whose elements are ids of leds to light up to create the words we need.
+//List of leds is terminated by -1 (invalid value)
+char ledsToLightUp[144];
 
 char charMatrix[12][12] = 
 {
@@ -16,7 +29,7 @@ char charMatrix[12][12] =
   {'Q','U','A','R','T','O','C','I','N','Q','U','E'}
 };
 
-const char* hours[] = {
+const char* hoursInWords[] = {
   "MEZZANOTTE",
   "^UNA",
   "LE DUE",
@@ -43,22 +56,70 @@ const char* hours[] = {
   "LE UNDICI"  
 };
 
-const char* minutes[] = {
+const char* minutesInWords[] = {
   "",
+  "E UNO",
+  "E DUE",
+  "E TRE",
+  "E QUATTRO",
   "E CINQUE",
+  "E SEI",
+  "E SETTE",
+  "E OTTO",
+  "E NOVE",
   "E DIECI",
+  "E UNDICI",
+  "E DODICI",
+  "E TREDICI",
+  "E QUATTORDICI",
   "E UN QUARTO",
+  "E SEDICI",
+  "E DICIASSETTE",
+  "E DICIOTTO",
+  "E DICIANNOVE",
   "E VENTI",
-  "E VENTICINQUE",
+  "E VENTUNO",
+  "E VENTI DUE",
+  "E VENTI TRE",
+  "E VENTI QUATTRO",
+  "E VENTI CINQUE",
+  "E VENTI SEI",
+  "E VENTI SETTE",
+  "E VENTOTTO",
+  "E VENTI NOVE",
   "E MEZZA",
+  "E TRENTUNO",
+  "E TRENTA DUE",
+  "E TRENTA TRE",
+  "E TRENTA QUATTRO",
   "E TRENTA CINQUE",
+  "E TRENTA SEI",
+  "E TRENTA SETTE",
+  "E TRENTOTTO",
+  "E TRENTA NOVE",
   "MENO VENTI",
+  "MENO DICIANNOVE",
+  "MENO DICIOTTO",
+  "MENO DICIASSETTE",
+  "MENO SEDICI",
   "MENO UN QUARTO",
+  "MENO QUATTORDICI",
+  "MENO TREDICI",
+  "MENO DODICI",
+  "MENO UNDICI",
   "MENO DIECI",
+  "MENO NOVE",
+  "MENO OTTO",
+  "MENO SETTE",
+  "MENO SEI",
   "MENO CINQUE"
+  "MENO QUATTRO",
+  "MENO TRE",
+  "MENO DUE",
+  "MENO UNO"
 };
 
-const char* templates[] = {
+const char* hourTemplates[] = {
   "! $ORE $MIN",
   "! $ORE $MIN",
   "SONO $ORE $MIN",
@@ -86,23 +147,62 @@ const char* templates[] = {
 };
 
 
-bool ledArray[144];
+const char ledArray[144];
 
-void setLedArray() 
+unsigned int lastTimeInMinutes = 0;
+
+String currentTimeAsWords(const int hours, const int minutes) 
+{
+  String tmpl = String(hourTemplates[hours]);
+  const char* minutesComponentInWords = minutesInWords[minutes];  
+  
+  const char* hoursComponentInWords = NULL;
+  // If minutes are more than 35 we must increase hours
+  int fixedHours = (minutes > 35)? hours + 1 : hours; 
+  hoursComponentInWords = hoursInWords[fixedHours];
+
+  tmpl.replace("$ORE", hoursComponentInWords);
+  tmpl.replace("$MIN",minutesComponentInWords);
+
+  return tmpl;
+}
+
+void fillLedsArrayNeededToWriteTime(const char* timeInWords, const char* ledArray, char matrix[12][12])
 {
   
 }
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(9600);
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  if (! rtc.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    // following line sets the RTC to the date & time this sketch was compiled
+     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+    DateTime now = rtc.now();
+    int currentHours = now.hour();
+    int currentMinutes = now.minute();
+    int currentTimeInMinutes = currentHours*60 + currentMinutes;
+    if ((lastTimeInMinutes == 0) || currentTimeInMinutes == lastTimeInMinutes + UPDATE_PERIOD_MINUTES) 
+    {
+      String currentTime = currentTimeAsWords(currentHours, currentMinutes);
+      lastTimeInMinutes = currentTimeInMinutes;
+      Serial.println(currentTime.c_str());
+      Serial.print(currentHours);
+      Serial.print(":");
+      Serial.println(currentMinutes);
+    }
+
+    delay(10000);
 }
